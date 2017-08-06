ea88be64e502d8877c5a126cfd4d4a4e6807861f
raone16 <raveenasenthilvel@gmail.com>
2017-08-06 16:29:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function (req,res){
-    res.send('Artical one will be available here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function (req,res){
     res.send('Artical two will be available here');
